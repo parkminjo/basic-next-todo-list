029fdd4ec28e30eaf558363c9c39d85209dc0738
parkminjo <cruiser367@naver.com>
2025-07-11 15:38:53 +0900

    feat: 홈 페이지에 상세 페이지 링크 추가 및 상세 페이지 컴포넌트 생성

---
+++ app/detail/[id]/page.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+const DetailPage = async ({ params }: Props) => {
+  const { id } = await params;
+  console.log(id);
+
+  return (
+    <div>
+      <Link href="/">go to HomePage</Link>
+    </div>
+  );
+};
+
+export default DetailPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Link href="/detail/1">go to DetailPage</Link>
+    </div>
+  );
 };
 
 export default HomePage;
