adfd8e4e9d59e674afd7a3ac8cdb30ebccbaae38
parkminjo <cruiser367@naver.com>
2025-07-15 13:39:31 +0900

    style: TodoItem 디자인 적용

--- components/todo/todo-item.tsx
+++ components/todo/todo-item.tsx
@@ -1,9 +1,11 @@
-'use client';
+"use client";
 
-import { deleteTodo, updateTodo } from '@/api/todo.api';
-import { ROUTER_PATH } from '@/constants/router-path';
-import { Todo } from '@/types/todo.type';
-import Link from 'next/link';
+import { deleteTodo, updateTodo } from "@/api/todo.api";
+import { ROUTER_PATH } from "@/constants/router-path";
+import { Todo } from "@/types/todo.type";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 interface Props {
   todo: Todo;
@@ -13,15 +15,21 @@ const TodoItem = ({ todo }: Props) => {
   const { id, content, completed } = todo;
 
   return (
-    <li className="flex justify-between">
-      <Link href={ROUTER_PATH.DETAIL(id)}>{content}</Link>
-      <p>{completed ? '완료' : '미완료'}</p>
+    <li className="flex flex-row items-center justify-between rounded-lg border px-4 py-2 transition-all">
+      <Link
+        href={ROUTER_PATH.DETAIL(id)}
+        className={cn({ "line-through": completed })}
+      >
+        {content}
+      </Link>
 
-      <div className="flex gap-1 flex-wrap">
-        <button onClick={() => updateTodo(id, completed)}>
-          {completed ? '취소' : '완료'}
-        </button>
-        <button onClick={() => deleteTodo(id)}>삭제</button>
+      <div className="flex flex-wrap gap-2">
+        <Button onClick={() => updateTodo(id, completed)} variant="outline">
+          {completed ? "취소" : "완료"}
+        </Button>
+        <Button onClick={() => deleteTodo(id)} variant="destructive">
+          삭제
+        </Button>
       </div>
     </li>
   );
