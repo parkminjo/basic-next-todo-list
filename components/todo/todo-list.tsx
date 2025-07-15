46a2273f4383451478fa9e771a5597c2f878ea8c
parkminjo <cruiser367@naver.com>
2025-07-15 13:39:40 +0900

    style: TodoList 디자인 적용

--- components/todo/todo-list.tsx
+++ components/todo/todo-list.tsx
@@ -1,11 +1,11 @@
-import { getTodoList } from '@/api/todo.api';
-import TodoItem from './todo-item';
+import { getTodoList } from "@/api/todo.api";
+import TodoItem from "./todo-item";
 
 const TodoList = async () => {
   const todoList = await getTodoList();
 
   return (
-    <ul>
+    <ul className="flex flex-col gap-4">
       {todoList.map((todo) => {
         return <TodoItem key={todo.id} todo={todo} />;
       })}
