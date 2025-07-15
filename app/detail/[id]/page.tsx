efbd2613b383d919a0a9d023fdcde786b4d64285
parkminjo <cruiser367@naver.com>
2025-07-15 13:47:10 +0900

    style: TodoDetailPage 디자인 수정

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,5 +1,8 @@
-import { getTodoById } from '@/api/todo.api';
-import TodoItem from '@/components/todo/todo-item';
+import { getTodoById } from "@/api/todo.api";
+import TodoItem from "@/components/todo/todo-item";
+import { Button } from "@/components/ui/button";
+import { ROUTER_PATH } from "@/constants/router-path";
+import Link from "next/link";
 
 interface Props {
   params: Promise<{ id: string }>;
@@ -9,7 +12,16 @@ const TodoDetailPage = async ({ params }: Props) => {
   const { id } = await params;
   const todo = await getTodoById(id);
 
-  return <TodoItem todo={todo} />;
+  return (
+    <section>
+      <div className="container mx-auto space-y-4">
+        <TodoItem todo={todo} />
+        <Link href={ROUTER_PATH.HOME}>
+          <Button className="w-full">돌아가기</Button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default TodoDetailPage;
