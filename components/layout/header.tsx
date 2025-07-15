a08d6432bdf9f2ed044efa6d5185d60ab40872ea
parkminjo <cruiser367@naver.com>
2025-07-15 13:16:43 +0900

    style: header 추가

---
+++ components/layout/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="mb-4">
+      <div className="container mx-auto py-4">
+        <h2 className="text-xl font-bold">TODO</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
