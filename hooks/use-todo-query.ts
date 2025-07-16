96db64c3261d0b001d5877d9d7d30ed04e882e62
parkminjo <cruiser367@naver.com>
2025-07-16 12:08:38 +0900

    feat: useTodoQuery 훅 생성

--- hooks/use-todo-query.ts
+++ hooks/use-todo-query.ts
@@ -1,10 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
-import { getTodoList } from "@/api/todo.api";
+import { getTodoById, getTodoList } from "@/api/todo.api";
 import { QUERY_KEY } from "@/constants/query-key";
+import type { Todo } from "@/types/todo.type";
 
 export const useTodoListQuery = () => {
   return useQuery({
     queryKey: [QUERY_KEY.TODO_LIST],
     queryFn: getTodoList,
+  });
+};
+
+export const useTodoQuery = (id: Todo["id"]) => {
+  return useQuery({
+    queryKey: [QUERY_KEY.TODO_LIST, id],
+    queryFn: () => getTodoById(id),
   });
 };
