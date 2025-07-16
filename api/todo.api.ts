c0d5e8567a2120d80212773eb618a5c1c4d7b42f
parkminjo <cruiser367@naver.com>
2025-07-16 11:55:46 +0900

    refactor: 불필요한 코드 제거

--- api/todo.api.ts
+++ api/todo.api.ts
@@ -1,13 +1,8 @@
-'use server';
+import { ENV } from "@/constants/env";
+import { TODO_ERROR_MESSAGE } from "@/constants/error-message";
+import { Todo } from "@/types/todo.type";
 
-import { ENV } from '@/constants/env';
-import { TODO_ERROR_MESSAGE } from '@/constants/error-message';
-import { Todo } from '@/types/todo.type';
-import { revalidateTag } from 'next/cache';
-
-const TAG_TODO_LIST = 'todoList';
-
-export const getTodoById = async (id: Todo['id']) => {
+export const getTodoById = async (id: Todo["id"]) => {
   try {
     const response = await fetch(`${ENV.JSON_SERVER}/${id}`);
     if (!response.ok) throw new Error(TODO_ERROR_MESSAGE.GET_TODO);
@@ -23,11 +18,7 @@ export const getTodoById = async (id: Todo['id']) => {
 
 export const getTodoList = async () => {
   try {
-    const response = await fetch(ENV.JSON_SERVER, {
-      next: {
-        tags: [TAG_TODO_LIST],
-      },
-    });
+    const response = await fetch(ENV.JSON_SERVER);
     if (!response.ok) throw new Error(TODO_ERROR_MESSAGE.GET_TODO_LIST);
 
     const todoList: Todo[] = await response.json();
@@ -39,32 +30,28 @@ export const getTodoList = async () => {
   }
 };
 
-export const createTodo = async (content: Todo['content']) => {
+export const createTodo = async (content: Todo["content"]) => {
   try {
     const response = await fetch(ENV.JSON_SERVER, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ content, completed: false }),
     });
     if (!response.ok) throw new Error(TODO_ERROR_MESSAGE.POST);
-
-    revalidateTag(TAG_TODO_LIST);
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
 
-export const deleteTodo = async (id: Todo['id']) => {
+export const deleteTodo = async (id: Todo["id"]) => {
   try {
     const response = await fetch(`${ENV.JSON_SERVER}/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     });
     if (!response.ok) throw new Error(TODO_ERROR_MESSAGE.DELETE);
-
-    revalidateTag(TAG_TODO_LIST);
   } catch (error) {
     console.error(error);
     throw error;
@@ -72,20 +59,18 @@ export const deleteTodo = async (id: Todo['id']) => {
 };
 
 export const updateTodo = async (
-  id: Todo['id'],
-  completed: Todo['completed']
+  id: Todo["id"],
+  completed: Todo["completed"],
 ) => {
   try {
     const response = await fetch(`${ENV.JSON_SERVER}/${id}`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ completed: !completed }),
     });
     if (!response.ok) throw new Error(TODO_ERROR_MESSAGE.PATCH);
-
-    revalidateTag(TAG_TODO_LIST);
   } catch (error) {
     console.error(error);
     throw error;
