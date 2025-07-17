b04000f276d5e5ede1a3f23e1b5205d227e05679
parkminjo <cruiser367@naver.com>
2025-07-17 13:00:07 +0900

    feat: supabase key와 url 추가

--- constants/env.ts
+++ constants/env.ts
@@ -1,3 +1,5 @@
 export const ENV = {
   JSON_SERVER_URL: process.env.NEXT_PUBLIC_JSON_SERVER as string,
+  SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_UR as string,
+  SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
 };
