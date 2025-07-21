42b41bedc5c90a8e7c7389976f45635560c7d1d8
parkminjo <cruiser367@naver.com>
2025-07-21 15:49:10 +0900

    fix: export 방식 수정

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,21 +1,12 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { ROUTER_PATH } from "@/constants/router-path";
 
-export function LoginForm({
-  className,
-  ...props
-}: React.ComponentProps<"div">) {
+const LoginForm = ({ className, ...props }: React.ComponentProps<"div">) => {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -55,4 +46,6 @@ export function LoginForm({
       </Card>
     </div>
   );
-}
+};
+
+export default LoginForm;
