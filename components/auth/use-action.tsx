1d472cdab78c213bcc1230634cbde06066cf95ca
parkminjo <cruiser367@naver.com>
2025-07-21 16:07:19 +0900

    style: Dropdown Menu 컴포넌트 구현

---
+++ components/auth/use-action.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOutIcon, UserCircleIcon } from "lucide-react";
+
+const UserAction = ({
+  user,
+}: {
+  user: {
+    name: string;
+    email: string;
+    avatar: string;
+  };
+}) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="h-8- w-8 rounded-full grayscale">
+          <AvatarFallback className="rounded-lg">
+            <UserCircleIcon />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+        align="end"
+        sideOffset={4}
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <UserCircleIcon />
+            My Page
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOutIcon />
+          로그아웃
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserAction;
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import { ROUTER_PATH } from "@/constants/router-path";
+import UserAction from "../auth/use-action";
 
 const Header = () => {
   return (
     <header className="mb-4">
       <div className="container mx-auto flex flex-row py-4">
-        <Link href={ROUTER_PATH.HOME}>
-          <h2 className="text-xl font-bold">TODO</h2>
-        </Link>
+        <div className="flex w-full flex-row items-center justify-between">
+          <Link href={ROUTER_PATH.HOME}>
+            <h2 className="text-xl font-bold">TODO</h2>
+          </Link>
+          <UserAction />
+        </div>
       </div>
     </header>
   );
