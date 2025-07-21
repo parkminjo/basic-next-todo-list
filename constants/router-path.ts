90aa15c16055897709f192e1fabc0fda346c02cf
parkminjo <cruiser367@naver.com>
2025-07-21 15:43:09 +0900

    feat: 로그인, 회원가입 path 상수에 추가

--- constants/router-path.ts
+++ constants/router-path.ts
@@ -1,6 +1,8 @@
-import { Todo } from '@/types/todo.type';
+import { Todo } from "@/types/todo.type";
 
 export const ROUTER_PATH = {
-  HOME: '/',
-  DETAIL: (id: Todo['id']) => `/detail/${id}`,
+  HOME: "/",
+  DETAIL: (id: Todo["id"]) => `/detail/${id}`,
+  LOGIN: "/login",
+  SIGN_UP: "/sign-up",
 };
