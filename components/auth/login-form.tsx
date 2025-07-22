db36279059f3cfd68ba90ed038f2479a1916c6bf
parkminjo <cruiser367@naver.com>
2025-07-22 17:13:52 +0900

    refactor: 로그인 페이지에 react-hook-form 연결

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,48 +1,99 @@
-import { cn } from "@/lib/utils";
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { login } from "@/app/auth/action";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import Link from "next/link";
 import { ROUTER_PATH } from "@/constants/router-path";
-import { login } from "@/app/auth/action";
 
-const LoginForm = ({ className, ...props }: React.ComponentProps<"div">) => {
+const LoginForm = () => {
+  const formSchema = z.object({
+    email: z.string().email({
+      error: "잘못된 이메일 형식입니다.",
+    }),
+    password: z
+      .string()
+      .min(6, {
+        error: "비밀번호는 최소 6자 이상이어야 합니다.",
+      })
+      .max(50),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const formData = new FormData();
+    formData.append("email", values.email);
+    formData.append("password", values.password);
+
+    login(formData);
+  }
+
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div className='"flex gap-6" flex-col'>
       <Card>
         <CardHeader>
           <CardTitle className="text-center">로그인</CardTitle>
         </CardHeader>
         <CardContent>
-          <form action={login}>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" name="email" type="email" required />
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="이메일" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="비밀번호" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                로그인
+              </Button>
+              <div className="mt-4 text-center text-sm">
+                아직 계정이 없으신가요?{" "}
+                <Link
+                  href={ROUTER_PATH.SIGN_UP}
+                  className="underline underline-offset-4"
+                >
+                  Sign up
+                </Link>
               </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                </div>
-                <Input id="password" name="password" type="password" required />
-              </div>
-              <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
-              </div>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              아직 계정이 없으신가요?{" "}
-              <Link
-                href={ROUTER_PATH.SIGN_UP}
-                className="underline underline-offset-4"
-              >
-                Sign up
-              </Link>
-            </div>
-          </form>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
