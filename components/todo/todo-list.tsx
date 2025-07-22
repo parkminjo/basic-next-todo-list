ec784a918bd8e779a77ae1f4fb4fc4963df09aa8
parkminjo <cruiser367@naver.com>
2025-07-22 16:49:04 +0900

    style: 투두리스트에 애니메이션 적용

--- components/todo/todo-list.tsx
+++ components/todo/todo-list.tsx
@@ -4,16 +4,18 @@ import { useTodoListQuery } from "@/hooks/use-todo-query";
 import LoadingIndicator from "../common/loading-indicator";
 import TodoItem from "./todo-item";
 import { useTodoFilterStore } from "@/store/use-todo-filter-store";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const TodoList = () => {
   const { filter } = useTodoFilterStore();
   const { data: todoList, isPending, isError } = useTodoListQuery(filter);
+  const [parent] = useAutoAnimate();
 
   if (isPending) return <LoadingIndicator />;
   if (isError) return <div>에러가 발생했습니다.</div>;
 
   return (
-    <ul className="flex flex-col gap-4">
+    <ul ref={parent} className="flex flex-col gap-4">
       {todoList.map((todo) => {
         return <TodoItem key={todo.id} todo={todo} />;
       })}
