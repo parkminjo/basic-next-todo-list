d35bdb591f03b36e5ed3bacb56f1cb37481065b0
parkminjo <cruiser367@naver.com>
2025-07-22 16:25:05 +0900

    feat: 인증된 사용자가 아닌 경우 redirect 레이아웃 추가

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,16 @@
 import Header from "@/components/layout/header";
+import { ROUTER_PATH } from "@/constants/router-path";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 
-const MainLayout = ({ children }: PropsWithChildren) => {
+const MainLayout = async ({ children }: PropsWithChildren) => {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data?.user) {
+    redirect(ROUTER_PATH.LOGIN);
+  }
   return (
     <div>
       <Header />
